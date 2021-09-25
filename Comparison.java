c45a3271aa1c384209721b719fb7108e809922a9
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-25 22:14:42 +0530

    Added: to compare the lengths

--- Comparison.java
+++ Comparison.java
@@ -1,18 +1,22 @@
 package Linecomparison;
 
 public class Comparison {
-	double length(int x1,int y1,int x2,int y2) {
+		double length(int x1,int y1,int x2,int y2) {
 		double distance;
 		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		return distance;
 	}
 		public static void main(String[] args) {
-	Comparison line = new 	Comparison();
-		Double result1 = line.length(2, 5, 6, 7);
-		Double result2 = line.length(3, 6, 8, 9);
-		Double result3 = line.length(3, 6, 8, 9);
-		System.out.println("to check the equality of lengths are true or false:" + result1.equals(result2));
-		System.out.println("to check the equality of lengths are true or false:" + result2.equals(result3));
-		
+			Comparison line = new 	Comparison();
+			Double result1 = line.length(3, 6, 8, 9);
+			Double result2 = line.length(4, 6, 8, 9);
+			if(Double.compare(result1,result2) == 0) {
+				System.out.println("two results are equal");
+			}else if(Double.compare(result1, result2) < 0) {
+				System.out.println("result1 is lessthan result2");
+			}else {
+				System.out.println("result1 is greaterthan result2");
+		}
+			
 	}
 }
