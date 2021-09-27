16cac2db59056c743a0946c799edb31e2ee4729a
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-27 11:55:09 +0530

    Refactored: compare the lengths

--- Comparison.java
+++ Comparison.java
@@ -8,14 +8,16 @@ public class Comparison {
 	}
 		public static void main(String[] args) {
 			Comparison line = new 	Comparison();
-			Double result1 = line.length(3, 6, 8, 9);
-			Double result2 = line.length(4, 6, 8, 9);
-			if(Double.compare(result1,result2) == 0) {
+			Double length1 = line.length(3, 6, 8, 9);
+			Double length2 = line.length(4, 6, 8, 9);
+			if(Double.compare(length1,length2 ) == 0) {
 				System.out.println("two results are equal");
-			}else if(Double.compare(result1, result2) < 0) {
-				System.out.println("result1 is lessthan result2");
-			}else {
-				System.out.println("result1 is greaterthan result2");
+			}
+			else if(Double.compare(length1, length2 ) < 0) {
+				System.out.println("length1 is lessthan length2 ");
+			}
+			else {
+				System.out.println("length1 is greaterthan length2 ");
 		}
 			
 	}
