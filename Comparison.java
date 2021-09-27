37a536f2d3479f6220541c9d25345326db09ea5a
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-27 12:11:40 +0530

    Refactored: to check the equality of lengths true or false

--- Comparison.java
+++ Comparison.java
@@ -1,24 +1,17 @@
 package Linecomparison;
 
 public class Comparison {
-		double length(int x1,int y1,int x2,int y2) {
+	double length(int x1,int y1,int x2,int y2) {
 		double distance;
 		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		return distance;
 	}
 		public static void main(String[] args) {
 			Comparison line = new 	Comparison();
-			Double length1 = line.length(3, 6, 8, 9);
-			Double length2 = line.length(4, 6, 8, 9);
-			if(Double.compare(length1,length2 ) == 0) {
-				System.out.println("two results are equal");
-			}
-			else if(Double.compare(length1, length2 ) < 0) {
-				System.out.println("length1 is lessthan length2 ");
-			}
-			else {
-				System.out.println("length1 is greaterthan length2 ");
-		}
-			
-	}
+			Double length1 = line.length(2, 5, 6, 7);
+			Double length2 = line.length(3, 6, 8, 9);
+			Double length3 = line.length(3, 6, 8, 9);
+			System.out.println("to check the equality of lengths are true or false:" + length1.equals(length2));
+			System.out.println("to check the equality of lengths are true or false:" + length2.equals(length3));	
+	}		
 }
